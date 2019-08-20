39aad024a82cbcbf12e2b3ceae5b0c2a57f45dc1
ezkat <50319957+ezkat@users.noreply.github.com>
2019-08-20 15:02:13 +0300

    Set default languages

--- respa_admin/static_src/js/resourceFormLanguage.js
+++ respa_admin/static_src/js/resourceFormLanguage.js
@@ -39,6 +39,11 @@ export function toggleCurrentLanguage(language = undefined, input = null) {
   languagesToHide.forEach(language => hideLanguage(language, input));
   let $languageButtons = $('[name$="language-' + language + '"]');
   $languageButtons.each((i, input) => input.classList.add('language-item-selected'));
+  if (language === 'fi') {
+    toggleLanguage('sv');
+  } else if (language === 'sv') {
+    toggleLanguage('fi');
+  }
 }
 
 /*
