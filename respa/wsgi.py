676a0b17274e6e2259f9f6b1a9fccd48b625d1ff
ezkat <50319957+ezkat@users.noreply.github.com>
2019-08-28 11:59:37 +0300

    Update wsgi.py

--- respa/wsgi.py
+++ respa/wsgi.py
@@ -9,11 +9,26 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 import sys
+import environ
 
 from django.core.wsgi import get_wsgi_application
 
 if sys.prefix not in sys.path:
     sys.path.append(sys.prefix)
+env = environ.Env(
+    RESPA_PROJECT_PATH = (str, ''),
+    RESPA_APP_PATH = (str, '')
+)
+environ.Env.read_env()
+
+project_path = env('RESPA_PROJECT_PATH')
+app_path = env('RESPA_APP_PATH')
+
+if project_path and project_path not in sys.path:
+    sys.path.append(project_path)
+
+if app_path and app_path not in sys.path:
+    sys.path.append(app_path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "respa.settings")
 
