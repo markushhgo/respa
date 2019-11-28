28133506e4842774c9d4a3a2ea8f8cebea1e3493
ezkat <50319957+ezkat@users.noreply.github.com>
2019-11-28 13:36:02 +0200

    Revert "Media urls"
    This reverts commit 1115fb3498e8e8507fe403f96f3db10a94d8f494.

--- respa/urls.py
+++ respa/urls.py
@@ -69,4 +69,5 @@ if settings.RESPA_PAYMENTS_ENABLED:
     urlpatterns.extend([
         path('payments/', include(payment_urls))
     ])
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
