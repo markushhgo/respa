e2659ffcafcabdba81c4aa5b931c2c45a9244750
ezkat <kim.koivu@turku.fi>
2020-08-11 12:53:37 +0300

    OIDC hotfix

--- respa/providers/turku_oidc/oidc.py
+++ respa/providers/turku_oidc/oidc.py
@@ -1,7 +1,11 @@
 from helusers.oidc import resolve_user, ApiTokenAuthentication as HelusersApiTokenAuthentication
 from helusers.authz import UserAuthorization
+from helusers.user_utils import _try_create_or_update
 from rest_framework.exceptions import AuthenticationFailed
 from django.utils.translation import ugettext as _
+from django.conf import settings
+from rest_framework import exceptions
+from django.db import transaction, IntegrityError
 
 class ApiTokenAuthentication(HelusersApiTokenAuthentication):
     def authenticate(self, request):
@@ -13,8 +17,7 @@ class ApiTokenAuthentication(HelusersApiTokenAuthentication):
         except:
             return None
         self.validate_claims(payload)
-        user_resolver = self.settings.USER_RESOLVER  # Default: resolve_user
-        user = user_resolver(request, payload)
+        user = get_or_create_user(payload, True)
         auth = UserAuthorization(user, payload, self.settings)
 
         if self.settings.REQUIRE_API_SCOPE_FOR_AUTHENTICATION:
@@ -24,3 +27,17 @@ class ApiTokenAuthentication(HelusersApiTokenAuthentication):
                     _("Not authorized for API scope \"{api_scope}\"")
                     .format(api_scope=api_scope))
         return (user, auth)
+
+def get_or_create_user(payload, oidc=False):
+    user_id = payload.get('sub')
+    if not user_id:
+        msg = _('Invalid payload.')
+        raise exceptions.AuthenticationFailed(msg)
+    try_again = False
+    try:
+        user = _try_create_or_update(user_id, payload, oidc)
+    except IntegrityError:
+        try_again = True
+    if try_again:
+        user = _try_create_or_update(user_id, payload, oidc)
+    return user
