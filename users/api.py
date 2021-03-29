c1cd6696d0e4f9913f883304df896e67e243bfe0
ezkat <50319957+ezkat@users.noreply.github.com>
2021-03-29 13:21:42 +0300

    Don't raise KeyError (#69)

--- users/api.py
+++ users/api.py
@@ -84,8 +84,8 @@ class UserSerializer(serializers.ModelSerializer):
         user = self.context['request'].user
 
         if user.id != instance.id:
-            del data['birthdate']
-            del data['oid']
+            data.pop('birthdate', None)
+            data.pop('oid', None)
         return data
 
 
