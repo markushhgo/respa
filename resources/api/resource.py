b7851b6643ed0e51dd82fa0ec202484dc3fba96e
ezkat <50319957+ezkat@users.noreply.github.com>
2022-05-13 14:26:01 +0300

    Increase the limit for images, periods and tags (#180)

--- resources/api/resource.py
+++ resources/api/resource.py
@@ -1559,17 +1559,17 @@ class ResourceCreateSerializer(TranslatedModelSerializer):
         extra = (
             ('images', 
                 {'kwargs': { 'many': True, 'context': self.context, 'data': validated_data.pop('images', {}), },
-                    'validate': ( (lambda data: len(data) <= 5, _('Invalid length, max: 5')), ),
+                    'validate': ( (lambda data: len(data) <= 20, _('Invalid length, max: 20')), ),
                     'save_kw': { 'resource_fk': True, }, } ),
 
             ('tags',
                 {'kwargs': {'many': True, 'context': self.context, 'data': validated_data.pop('tags', []), },
-                    'validate': ( (lambda data: len(data) <= 20,  _('Invalid length, max: 20')), ),
+                    'validate': ( (lambda data: len(data) <= 100,  _('Invalid length, max: 100')), ),
                     'save_kw': { 'resource_fk': True }, } ),
 
             ('periods', 
                 { 'kwargs': { 'many': True, 'context': self.context, 'data': validated_data.pop('periods', {}), },
-                    'validate': ( (lambda data: len(data) <= 10,  _('Invalid length, max: 10')), ),
+                    'validate': ( (lambda data: len(data) <= 20,  _('Invalid length, max: 20')), ),
                     'save_kw': { 'resource_fk': True }, } ),
     
             ('equipments', 
